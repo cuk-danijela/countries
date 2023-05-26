82dec15e4b0731b9a33d923238d3f586f7da2ba8
cuk-danijela <danijela@tcom.rs>
2023-05-26 22:43:49 +0200

    first init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Countries from './components/Countries/Countries'
+import Country from './components/Country/Country'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <div className="header">
+        <div className="container">
+          <h5>Where in the world</h5>
+        </div>
+      </div>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route path='/' element={<Countries />} />
+            <Route path='/country/:countryName' element={<Country />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
+  )
+}
